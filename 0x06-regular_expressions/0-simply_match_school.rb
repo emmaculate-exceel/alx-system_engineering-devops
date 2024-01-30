3c4727f71759d7915c815c9e0400355342cb0085
emmaculate-exceel <emmaculate2exceel@live.com>
2024-01-30 17:18:38 +0100

    School

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#trying to match a word expression
+#using REGEX "regular expression"
+#the scripting language used is Ruby
+
+/\\wScho+/
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,2 @@
+#I never knew ruby was a scripting language 
+Regular expressions using Ruby scripting language

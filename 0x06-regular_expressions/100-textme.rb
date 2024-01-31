8c4ad932efb9a6653e0176aabf9cd46c83571334
emmaculate-exceel <emmaculate2exceel@live.com>
2024-01-31 17:31:51 +0100

    text log

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 #search for info in a message log
 #a log of [sender] [reciever] [flags]
 
-puts ARGV[0].scan(//).join(',')
+puts ARGV[0].scan(/from:[0-9]{11}|to:[0-9]{11}|flags:.\d.\d..\d.\d..\d/).join(',')

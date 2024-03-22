20ed7800bdd344c7d7818fdb5c25dd12ca75e8bd
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-22 18:37:55 +0100

    config&readme

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 ###create a file using puppet
 
-file {"/tmp/school":
-     ensure => present,
-     mode => '0744',
-     owner => 'www-data',
-     group => 'www-data',
-     content => 'I love Puppet',
+file {'/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }

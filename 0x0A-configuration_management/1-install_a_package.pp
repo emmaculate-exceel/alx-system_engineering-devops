8d48bd985a11b7984f0abe8a9f537da575b700ab
emmaculate-exceel <emmaculate2exceel@live.com>
2024-03-22 19:58:16 +0100

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,9 @@
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  before   => Exec['verify_flask_version'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

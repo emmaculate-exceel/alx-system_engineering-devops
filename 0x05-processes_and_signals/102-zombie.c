28013702068186fad5d8ef3a2c372174183766d1
emmaculate-exceel <emmaculate2exceel@live.com>
2025-07-13 07:35:30 +0100

    zombies created

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -11,7 +11,7 @@
 
 int infinite_while(void)
 {
-	while (1)
+	while(1)
 		{
 			sleep(1);
 		}
@@ -30,14 +30,14 @@ int main(void)
 	pid_t pid;
 	int i;
 
-	for (i = 0; i < 5; i++)
+	for(i = 0; i < 5; i++)
 		{
 			pid = fork();
-			if (pid > 0)
+			if(pid > 0)
 				{
 					printf("Zombie process created, PID %d\n", pid);
 				}
-			else if (pid == 0)
+			else if(pid == 0)
 				{
 					exit(0);
 				}

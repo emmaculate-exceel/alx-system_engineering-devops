1a0f56d148842eede5939f1dbf3820dd92d06ec3
emmaculate-exceel <emmaculate2exceel@live.com>
2025-07-13 07:21:15 +0100

    zombies created

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,18 +4,18 @@
 #include <sys/types.h>
 
 /**
- * infinite_while - Entry point 
+ * infinite_while - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int infinite_while(void)
 {
-  while (1)
-    {
-      sleep(1);
-    }
-  return (0);
+	while (1)
+		{
+			sleep(1);
+		}
+	return (0);
 }
 
 
@@ -27,26 +27,26 @@ int infinite_while(void)
 
 int main(void)
 {
-  pid_t pid;
-  int i;
+	pid_t pid;
+	int i;
 
-  for (i = 0; i < 5; i++)
-    {
-      pid = fork();
-      if (pid > 0)
-        {
-          printf("Zombie process created, PID %d\n", pid);
-        }
-      else if (pid == 0)
-        {
-          exit(0);
-        }
-      else
-        {
-          perror("fork");
-          return (1);
-        }
-    }
-  infinite_while();
-  return (0);
+	for (i = 0; i < 5; i++)
+		{
+			pid = fork();
+			if (pid > 0)
+				{
+					printf("Zombie process created, PID %d\n", pid);
+				}
+			else if (pid == 0)
+				{
+					exit(0);
+				}
+			else
+				{
+					perror("fork");
+					return (1);
+				}
+		}
+	infinite_while();
+	return (0);
 }

5753819176f65eb06e6996e2cfe2e8b6046f4ebd
emmaculate-exceel <emmaculate2exceel@live.com>
2025-07-13 12:34:08 +0100

    betty almost finished it

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -11,7 +11,7 @@
 
 int infinite_while(void)
 {
-	while(1)
+	while (1)
 	{
 		sleep(1);
 	}
@@ -30,23 +30,23 @@ int main(void)
 	pid_t pid;
 	int i;
 
-	for(i = 0; i < 5; i++)
-		{
-			pid = fork();
-			if(pid > 0)
-				{
-					printf("Zombie process created, PID %d\n", pid);
-				}
-			else if(pid == 0)
-				{
-					exit(0);
-				}
-			else
-				{
-					perror("fork");
-					return (1);
-				}
-		}
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+	if (pid > 0)
+	{
+		printf("Zombie process created, PID %d\n", pid);
+	}
+	else if (pid == 0)
+	{
+		exit(0);
+	}
+	else
+	{
+		perror("fork");
+		return (1);
+	}
+	}
 	infinite_while();
 	return (0);
 }

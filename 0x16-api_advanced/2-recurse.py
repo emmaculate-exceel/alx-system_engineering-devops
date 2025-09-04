33090ce7c3da421f4cb4976dbe634c378b7cec3c
emmaculate-exceel <emmaculate2exceel@live.com>
2025-09-04 18:12:53 +0100

    recurse 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,12 @@
-#!/usr/
+#!/usr/bin/python3
+"""
+recursive function using Reddit API
+listing all articles and titles
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """functions that prints hot list articles"""
+    urls = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = 

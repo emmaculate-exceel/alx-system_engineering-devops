90ec49b3585430d7e606ed5b8daf60625f1c3e7a
emmaculate-exceel <emmaculate2exceel@live.com>
2025-09-12 17:34:42 +0100

    recurse final

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,33 @@ import requests
 def recurse(subreddit, hot_list=[]):
     """functions that prints hot list articles"""
     urls = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "recursive-reddit-fetcher/0.1"}
+    params = {"limit": 100}
 
+    # pagination token if present
+    if after:
+        params['after'] = after
 
+    # Make the request witihout following redirects
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    except requests.RequestException:
+        return None
 
-    
+    # Check for valid response
+    if response.status_code != 200:
+        return None
+
+    data = reponse.json().get('data', {})
+    children = data.get('children', [])
+
+    # Append titles to hot_list
+    for post in children:
+        hot_list.append(post['data']['title'])
+
+    # Recursively fetch next page if available
+    after = data.get('after')
+    if after:
+        return recurse(subreddit, hot_list, after)
+
+    return hot_list if hot_list else None
